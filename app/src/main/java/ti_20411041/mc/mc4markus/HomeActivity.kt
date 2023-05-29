886f9ec853b7078b10460d003678cb431990c792
Maku1619 <markuspaide@gmail.com>
2023-05-29 22:11:48 +0700

    first commit

---
+++ app/src/main/java/ti_20411041/mc/mc4markus/HomeActivity.kt
@@ -0,0 +1,11 @@
+package ti_20411041.mc.mc4markus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
